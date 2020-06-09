b2a2f86ce82bca601c69b1aca184d0cd16d693b1
Ian Filipe <ssianfilipe@gmail.com>
2020-06-09 11:28:10 -0300

    Improve cal and variables declaration

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const weightElement = document.querySelector('#weight')
 const heightElement = document.querySelector('#height')
 const calcElement = document.querySelector('#calc')
 const resultElement = document.querySelector('#result')
-let name = ""
-let weight = 0
-let height = 0
+let name, weight, height, result
 
 nameElement.addEventListener('input', () => { 
     name = nameElement.value
@@ -18,10 +16,19 @@ heightElement.addEventListener('input', () => {
 })
 
 calcElement.addEventListener('click', () => {
-    let result = weight / height ** 2
-    if (result > 30) {
+    result = weight / height ** 2
+    
+    if (result < 18.5) {
+        resultElement.value=`${name}, you're underweight.`
+    } else if (result < 24.9) {
+        resultElement.value=`${name}, you're at normal weight.`
+    } else if (result < 29.9) {
         resultElement.value=`${name}, you're overweight.`
+    } else if (result < 34.9) {
+        resultElement.value=`${name}, you're with severe obesity.`
+    } else if (result < 39.9) {
+        resultElement.value=`${name}, you're with morbid obesity.`
     } else {
-        resultElement.value=`${name}, you're not overweight.`
+        resultElement.value=`${name}, you're with super obesity.`
     }
 })
